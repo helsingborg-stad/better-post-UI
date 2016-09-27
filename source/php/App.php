90961dfdfacb29a0ec2e743d51595c44cf9d3cf7
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-27 10:50:38 +0200

    Initial

---
+++ source/php/App.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BetterPostUi;
+
+class App
+{
+    public function __construct()
+    {
+        add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
+    }
+
+    /**
+     * Enqueue required style
+     * @return void
+     */
+    public function enqueueStyles()
+    {
+        wp_enqueue_style('better-post-ui', BETTERPOSTUI_URL . '/dist/css/better-post-ui.min.css', null, '1.0.0');
+    }
+
+    /**
+     * Enqueue required scripts
+     * @return void
+     */
+    public function enqueueScripts()
+    {
+        wp_enqueue_script('better-post-ui', BETTERPOSTUI_URL . '/dist/js/better-post-ui.min.js', null, '1.0.0', true);
+    }
+}
