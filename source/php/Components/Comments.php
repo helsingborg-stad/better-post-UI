80a15479f05d83d36c3fb8fa70266a2b3247f31d
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-21 10:41:02 +0200

    Added table column "post type" to comments

--- source/php/Components/Comments.php
+++ source/php/Components/Comments.php
@@ -8,6 +8,8 @@ class Comments {
 	{
 		add_action('restrict_manage_comments', array($this, 'commentsFilter'));
 		add_action('pre_get_comments', array($this, 'queryFilter'));
+		add_filter('manage_edit-comments_columns', array($this, 'tableColumns'));
+        add_action('manage_comments_custom_column', array($this, 'tableColumnsContent'), 10, 2);
 	}
 
     /**
@@ -52,5 +54,38 @@ class Comments {
         }
 
         $query->set('post_type', $_GET['post_type']);
+    }
+
+    /**
+     * Table columns
+     * @param  array $columns
+     * @return array
+     */
+    public function tableColumns($columns)
+    {
+        return array(
+            'cb' => '',
+            'author' => __('Author'),
+            'comment' => __('Comment'),
+            'post_type' =>  __('Post type', 'better-post-ui'),
+            'response' => __('Response', 'better-post-ui'),
+            'date' => __('Date')
+        );
+    }
+
+    /**
+     * Content for table columns
+     * @param  string $column
+     * @param  int $postId
+     * @return void
+     */
+    public function tableColumnsContent($column, $postId)
+    {
+    	if ($column == 'post_type') {
+    		$comment = get_comment($postId, OBJECT);
+    		$post_type_slug = get_post_type($comment->comment_post_ID);
+    		$post_type_obj = get_post_type_object($post_type_slug);
+    		echo $post_type_obj->label;
+    	}
     }
 }
