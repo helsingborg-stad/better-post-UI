cd6675e436bfc916acf0f5fd15439104323a2014
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-21 13:08:20 +0200

    Ability to filter media by PDF

--- source/php/App.php
+++ source/php/App.php
@@ -14,6 +14,7 @@ class App
         new \BetterPostUi\Components\Order();
         new \BetterPostUi\Components\InternalLinks();
         new \BetterPostUi\Components\Comments();
+        new \BetterPostUi\Components\Media();
     }
 
     public function isEditPage()
---
+++ source/php/Components/Media.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BetterPostUi\Components;
+
+class Media
+{
+    public function __construct()
+    {
+    	add_filter('post_mime_types', array($this, 'customMimeTypes'));
+    }
+
+    /**
+     * Add custom sortable media mime type
+     * @param  array $post_mime_types Default list of post mime types
+     * @return array                  Modified list
+     */
+    public function customMimeTypes($post_mime_types)
+    {
+        $post_mime_types['application/pdf'] = array(__('PDF'), __('Manage PDF', 'better-post-ui'), _n_noop('PDF <span class="count">(%s)</span>', 'PDF <span class="count">(%s)</span>'));
+        return $post_mime_types;
+    }
+}
