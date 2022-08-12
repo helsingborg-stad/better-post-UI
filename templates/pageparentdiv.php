9c7315bbc8fd2200ecd95481937a0ac4c668b6bf
Mikael Strömgren <mikael.stromgren@frontwalker.se>
2022-08-12 14:19:16 +0200

    Avoid double display of template selector in Gutenberg

--- templates/pageparentdiv.php
+++ templates/pageparentdiv.php
@@ -4,7 +4,7 @@ if ($postTypeObject->hierarchical) {
     include('attributes/parent.php');
 }
 
-if (count(get_page_templates($post)) > 0 && get_option('page_for_posts') != $post->ID) {
+if (count(get_page_templates($post)) > 0 && get_option('page_for_posts') != $post->ID && !get_current_screen()->is_block_editor()) {
     include('attributes/template.php');
 }
 
